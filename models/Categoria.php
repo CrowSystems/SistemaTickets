233ca9b27e077ba7fef05792be43ae328fe41096
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2025-11-21 20:37:44 -0600

    Conexión de con sql}

---
+++ models/Categoria.php
@@ -0,0 +1,13 @@
+<?php
+    class Categoria extends Conectar{
+        /*TODO: Listar Registros */
+        public function get_categoria_x_suc_id(){
+
+        }
+
+        /*TODO: Listar Registro por ID en especifico */
+        public function get_categoria_x_cat_id(){
+            
+        }
+    }
+?>
