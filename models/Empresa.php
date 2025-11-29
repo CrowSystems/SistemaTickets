dd5692c75e75ce2b56ab739221b4f9594440529b
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2025-11-28 21:37:05 -0600

    Modelo y Procedimiento almacenado de Empresa

--- models/Empresa.php
+++ models/Empresa.php
@@ -1,49 +1,54 @@
 <?php
-    class Compania extends Conectar{
+    class Empresa extends Conectar{
         // TODO: Listar Registros
-        public function get_compania(){
+        public function get_empresa_x_com_id($com_id){
             $conectar=parent::Conexion();
-            $sql="SP_L_COMPANIA_01";
+            $sql="SP_L_EMPRESA_01 ?";
             $query=$conectar->prepare($sql);
+            $query->bindValue(1,$com_id);
             $query->execute();
             return $query->fetchAll(PDO::FETCH_ASSOC);
         }
 
         // TODO: Listar Registo por ID en especifico
-        public function get_compania_x_com_id($com_id){
+        public function get_empresa_x_emp_id($emp_id){
             $conectar=parent::Conexion();
-            $sql="SP_L_COMPANIA_02 ?";
+            $sql="SP_L_EMPRESA_02 ?";
             $query=$conectar->prepare($sql);
-            $query->bindValue(1,$com_id);
+            $query->bindValue(1,$emp_id);
             $query->execute();
             return $query->fetchAll(PDO::FETCH_ASSOC);
         }
 
         // TODO: Eliminar o cambiar estado a eliminado
-        public function delete_compania($com_id){
+        public function delete_empresa($emp_id){
             $conectar=parent::Conexion();
-            $sql="SP_D_COMPANIA_01";
+            $sql="SP_D_EMPRESA_01";
             $query=$conectar->prepare($sql);
-            $query->bindValue(1,$com_id);
+            $query->bindValue(1,$emp_id);
             $query->execute();
         }
 
         // TODO: Registro de datos
-        public function insert_compania($com_nom){
+        public function insert_empresa($com_id,$emp_nom,$emp_ruc){
             $conectar=parent::Conexion();
-            $sql="SP_I_COMPANIA_01 ?";
+            $sql="SP_I_EMPRESA_01 ?,?,?";
             $query=$conectar->prepare($sql);
-            $query->bindValue(1,$com_nom);
+            $query->bindValue(1,$com_id);
+            $query->bindValue(2,$emp_nom);
+            $query->bindValue(3,$emp_ruc);
             $query->execute();
         }
 
         // TODO: Actualizar Datos
-        public function update_compania($com_id,$com_nom){
+        public function update_empresa($emp_id,$com_id,$emp_nom,$emp_ruc){
             $conectar=parent::Conexion();
-            $sql="SP_I_COMPANIA_01 ?,?";
+            $sql="SP_I_EMPRESA_01 ?,?,?,?";
             $query=$conectar->prepare($sql);
-            $query->bindValue(1,$com_id);
-            $query->bindValue(2,$com_nom);
+            $query->bindValue(1,$emp_id);
+            $query->bindValue(2,$com_id);
+            $query->bindValue(3,$emp_nom);
+            $query->bindValue(3,$emp_ruc);
             $query->execute();
         }
     }
