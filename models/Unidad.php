80f99ef2975f303fff7f8e939b714f0f337dbc15
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2025-11-28 19:58:22 -0600

    carga de modelo de Unidad

--- models/Unidad.php
+++ models/Unidad.php
@@ -1,9 +1,9 @@
 <?php
-    class Categoria extends Conectar{
+    class Unidad extends Conectar{
         // TODO: Listar Registros
-        public function get_categoria_x_suc_id($suc_id){
+        public function get_unidad_x_suc_id($suc_id){
             $conectar=parent::Conexion();
-            $sql="SP_L_CATEGORIA_01 ?";
+            $sql="SP_L_UNIDAD_01 ?";
             $query=$conectar->prepare($sql);
             $query->bindValue(1,$suc_id);
             $query->execute();
@@ -11,9 +11,9 @@
         }
 
         // TODO: Listar Registo por ID en especifico
-        public function get_categoria_x_cat_id($cat_id){
+        public function get_unidad_x_cat_id($cat_id){
             $conectar=parent::Conexion();
-            $sql="SP_L_CATEGORIA_02 ?";
+            $sql="SP_L_UNIDAD_02 ?";
             $query=$conectar->prepare($sql);
             $query->bindValue(1,$cat_id);
             $query->execute();
@@ -21,16 +21,16 @@
         }
 
         // TODO: Eliminar o cambiar estado a eliminado
-        public function delete_categoria($cat_id){
+        public function delete_unidad($cat_id){
             $conectar=parent::Conexion();
-            $sql="SP_D_CATEGORIA_01";
+            $sql="SP_D_UNIDAD_01";
             $query=$conectar->prepare($sql);
             $query->bindValue(1,$cat_id);
             $query->execute();
         }
 
         // TODO: Registro de datos
-        public function insert_categoria($suc_id,$cat_nom){
+        public function insert_unidad($suc_id,$cat_nom){
             $conectar=parent::Conexion();
             $sql="SP_I_UNIDAD_01 ?,?";
             $query=$conectar->prepare($sql);
@@ -40,7 +40,7 @@
         }
 
         // TODO: Actualizar Datos
-        public function update_categoria($cat_id,$suc_id,$cat_nom){
+        public function update_unidad($cat_id,$suc_id,$cat_nom){
             $conectar=parent::Conexion();
             $sql="SP_I_UNIDAD_01 ?,?,?";
             $query=$conectar->prepare($sql);
