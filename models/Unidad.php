d013a48c3bdadc3ef7d2081ee09e1dd8baa090d1
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2025-11-28 20:06:09 -0600

    Modelo de Moneda PHP

--- models/Unidad.php
+++ models/Unidad.php
@@ -11,42 +11,42 @@
         }
 
         // TODO: Listar Registo por ID en especifico
-        public function get_unidad_x_cat_id($cat_id){
+        public function get_unidad_x_und_id($und_id){
             $conectar=parent::Conexion();
             $sql="SP_L_UNIDAD_02 ?";
             $query=$conectar->prepare($sql);
-            $query->bindValue(1,$cat_id);
+            $query->bindValue(1,$und_id);
             $query->execute();
             return $query->fetchAll(PDO::FETCH_ASSOC);
         }
 
         // TODO: Eliminar o cambiar estado a eliminado
-        public function delete_unidad($cat_id){
+        public function delete_unidad($und_id){
             $conectar=parent::Conexion();
             $sql="SP_D_UNIDAD_01";
             $query=$conectar->prepare($sql);
-            $query->bindValue(1,$cat_id);
+            $query->bindValue(1,$und_id);
             $query->execute();
         }
 
         // TODO: Registro de datos
-        public function insert_unidad($suc_id,$cat_nom){
+        public function insert_unidad($suc_id,$und_nom){
             $conectar=parent::Conexion();
             $sql="SP_I_UNIDAD_01 ?,?";
             $query=$conectar->prepare($sql);
             $query->bindValue(1,$suc_id);
-            $query->bindValue(2,$cat_nom);
+            $query->bindValue(2,$und_nom);
             $query->execute();
         }
 
         // TODO: Actualizar Datos
-        public function update_unidad($cat_id,$suc_id,$cat_nom){
+        public function update_unidad($und_id,$suc_id,$und_nom){
             $conectar=parent::Conexion();
             $sql="SP_I_UNIDAD_01 ?,?,?";
             $query=$conectar->prepare($sql);
-            $query->bindValue(1,$cat_id);
+            $query->bindValue(1,$und_id);
             $query->bindValue(2,$suc_id);
-            $query->bindValue(3,$cat_nom);
+            $query->bindValue(3,$und_nom);
             $query->execute();
         }
     }
